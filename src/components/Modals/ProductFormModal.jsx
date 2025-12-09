d4a2c80555eac0c4bec8975be5b0d31aff987779
yogi fernanda <moh.yogi10065@gmail.com>
2025-12-09 15:05:05 +0700

    fixing not responsive add modal

--- src/components/Modals/ProductFormModal.jsx
+++ src/components/Modals/ProductFormModal.jsx
@@ -158,7 +158,7 @@ const ProductFormModal = ({ isOpen, closeModal, product, onSave }) => {
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+      <Dialog as="div" className="relative z-50" onClose={closeModal}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
